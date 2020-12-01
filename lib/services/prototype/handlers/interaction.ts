b9df010dff4f34c617e104be2695ccd9b2261bf4
Tyler Han <tyler@voiceflow.com>
2020-11-30 19:57:29 -0500

    fix: choice block

--- lib/services/prototype/handlers/interaction.ts
+++ lib/services/prototype/handlers/interaction.ts
@@ -37,7 +37,7 @@ export const InteractionHandler: HandlerFactory<Node, typeof utilsObj> = (utils)
       return node.id;
     }
 
-    let nextId: string | null = null;
+    let nextId: string | null | undefined;
     let variableMap: SlotMapping[] | null = null;
 
     const { intent } = request.payload;
@@ -58,7 +58,7 @@ export const InteractionHandler: HandlerFactory<Node, typeof utilsObj> = (utils)
     }
 
     // check if there is a command in the stack that fulfills intent
-    if (!nextId) {
+    if (nextId === undefined) {
       if (utils.commandHandler.canHandle(context)) {
         return utils.commandHandler.handle(context, variables);
       }
@@ -71,14 +71,14 @@ export const InteractionHandler: HandlerFactory<Node, typeof utilsObj> = (utils)
     context.turn.delete(TurnType.REQUEST);
 
     // check for noMatches to handle
-    if (!nextId && utils.noMatchHandler.canHandle(node, context)) {
+    if (nextId === undefined && utils.noMatchHandler.canHandle(node, context)) {
       return utils.noMatchHandler.handle(node, context, variables);
     }
 
     // clean up no matches counter
     context.storage.delete(StorageType.NO_MATCHES_COUNTER);
 
-    return (nextId || node.elseId) ?? null;
+    return (nextId !== undefined ? nextId : node.elseId) || null;
   },
 });
 
