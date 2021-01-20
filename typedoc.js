260fa25e64a2c05904b7e3e91d59a57fdb29161a
leartgjoni-voiceflow <53058131+leartgjoni-voiceflow@users.noreply.github.com>
2021-01-20 10:55:06 -0500

    feat: docs generation (#27)

---
+++ typedoc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  out: './docs/',
+  name: 'Voiceflow General Runtime',
+  includeVersion: true,
+  tsconfig: 'tsconfig.build.json',
+  readme: 'README.md',
+  excludeExternals: true,
+  excludePrivate: true,
+  hideGenerator: true,
+  media: './assets/media',
+};
