84bbffa767e6b3dd9fcd50d256d5a6de9ce30bec
Justin Goping <32006038+jgoping@users.noreply.github.com>
2021-01-26 17:10:32 -0500

    fix: account for dm prefix being attached to entity values (CORE-4959) (#34)
    * fix: shorten hash value
    
    * fix: account for dm prefix being attached to entity values
    
    * fix: simplify implementation

--- lib/services/dialog/index.ts
+++ lib/services/dialog/index.ts
@@ -102,9 +102,15 @@ class DialogManagement extends AbstractManager<{ utils: typeof utils }> implemen
       const { query } = incomingRequest.payload;
 
       try {
+        const prefix = dmPrefix(dmStateStore.intentRequest.payload.intent.name);
         const dmPrefixedResult = await this.services.nlu.predict({
-          query: `${dmPrefix(dmStateStore.intentRequest.payload.intent.name)} ${query}`,
+          query: `${prefix} ${query}`,
           projectID: version.projectID,
+        });
+
+        // Remove the dmPrefix from entity values that it has accidentally been attached to
+        dmPrefixedResult.payload.entities.forEach((entity) => {
+          entity.value = entity.value.replace(prefix, '').trim();
         });
 
         const isFallback = this.handleDMContext(dmStateStore, dmPrefixedResult, incomingRequest, version.prototype.model);
