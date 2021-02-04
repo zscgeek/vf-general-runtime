e435b96612db78ac328e7e0c15879d230f595039
leartgjoni-voiceflow <leart@voiceflow.com>
2021-02-04 14:03:07 -0500

    chore: comment on speak block

--- lib/services/runtime/handlers/speak.ts
+++ lib/services/runtime/handlers/speak.ts
@@ -21,6 +21,7 @@ const SpeakHandler: HandlerFactory<Node> = () => ({
     const sanitizedVars = sanitizeVariables(variables.getState());
 
     if (_.isString(speak)) {
+      // in case a variable's value is a text containing another variable (i.e text2="say {text}")
       const output = replaceVariables(replaceVariables(speak, sanitizedVars), sanitizedVars);
 
       runtime.storage.produce((draft) => {
