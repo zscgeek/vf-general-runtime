19ec0c074f78f07696f74c6221cad751a7c06114
Ben Teichman <ben@voiceflow.com>
2021-03-02 19:10:12 -0500

    chore: add LAN IP to generated cert hosts

--- scripts/generate_certs.sh
+++ scripts/generate_certs.sh
@@ -10,7 +10,8 @@ fi
 
 rm -rf certs/
 mkdir -p certs/ && cd certs/
-$(brew --prefix mkcert)/bin/mkcert localhost 127.0.0.1 ::1
+LAN_IP=$(ifconfig en0 | grep "inet " | awk '{print $2}')
+$(brew --prefix mkcert)/bin/mkcert localhost 127.0.0.1 ::1 $LAN_IP
 
 # Rename certificate and key
 mv *-key.pem localhost.key
