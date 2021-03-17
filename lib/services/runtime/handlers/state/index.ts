f5b8da03be03c7c2679cdcc7d8ccbbdb44abc6f2
leartgjoni-voiceflow <53058131+leartgjoni-voiceflow@users.noreply.github.com>
2021-03-17 11:44:31 -0400

    feat: preliminary handler (CORE-4993) (#79)
    * wip: preliminary handler
    
    * wip: preliminary handler
    
    * test: unit

--- lib/services/runtime/handlers/state/index.ts
+++ lib/services/runtime/handlers/state/index.ts
@@ -1,4 +1,5 @@
 import oneShotHandler from './oneShot';
+import preliminaryHandler from './preliminary';
 import streamStateHandler from './stream';
 
-export default () => [streamStateHandler(), oneShotHandler()];
+export default () => [streamStateHandler(), oneShotHandler(), preliminaryHandler()];
