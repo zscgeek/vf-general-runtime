1c091c96b37edfec73564374937e85c0abe8ca7b
Tyler Han <tylerhan97@gmail.com>
2021-04-30 16:27:00 -0400

    fix: rate limit origin (VF-000) (#109)

--- lib/middlewares/rateLimit.ts
+++ lib/middlewares/rateLimit.ts
@@ -3,11 +3,13 @@ import { NextFunction, Request, Response } from 'express';
 
 import { AbstractMiddleware } from './utils';
 
+const LOCAL_DEVELOPEMENT = 'https://creator-local.development.voiceflow.com:3002';
+
 class RateLimit extends AbstractMiddleware {
   async verify(req: Request<{}>, _res: Response, next: NextFunction) {
     if (
       !this.config.PROJECT_SOURCE &&
-      (!this.config.CREATOR_APP_ORIGIN || req.headers.origin !== this.config.CREATOR_APP_ORIGIN) &&
+      ![this.config.CREATOR_APP_ORIGIN, LOCAL_DEVELOPEMENT].includes(req.headers.origin || 'no-origin') &&
       !req.headers.authorization
     ) {
       throw new VError('Auth Key Required', VError.HTTP_STATUS.UNAUTHORIZED);
