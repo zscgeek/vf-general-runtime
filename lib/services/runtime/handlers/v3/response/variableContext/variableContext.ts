2b7ead83b4b6d8cbac1297bb84a0feeafe23b029
Brennan Lu <brennan.lu314@gmail.com>
2023-07-19 15:13:03 -0400

    feat: added variable context class

---
+++ lib/services/runtime/handlers/v3/response/variableContext/variableContext.ts
@@ -0,0 +1,63 @@
+import { BaseMarkup as ResolvedMarkup } from '@voiceflow/base-types';
+import { replaceVariables } from '@voiceflow/common/build/cjs/utils';
+
+import { EntityReference, Markup, MarkupNode, MarkupSpan, VariableReference } from '../response.types';
+
+export class VariableContext {
+  constructor(
+    private readonly variables: Record<string, unknown>,
+    private readonly entities: Record<string, unknown>
+  ) {}
+
+  private isVariableReference(markupNode: MarkupNode): markupNode is VariableReference {
+    return VariableReference.safeParse(markupNode).success;
+  }
+
+  private isEntityReference(markupNode: MarkupNode): markupNode is EntityReference {
+    return EntityReference.safeParse(markupNode).success;
+  }
+
+  private isMarkupSpan(markupNode: MarkupNode): markupNode is MarkupSpan {
+    return MarkupSpan.safeParse(markupNode).success;
+  }
+
+  private resolveMarkupNode(markupNode: MarkupNode): ResolvedMarkup.MarkupNode {
+    if (typeof markupNode === 'string') {
+      return markupNode;
+    }
+
+    if (this.isVariableReference(markupNode)) {
+      const variableValue = this.variables[markupNode.variableID];
+      return JSON.stringify(variableValue);
+    }
+
+    if (this.isEntityReference(markupNode)) {
+      const variableValue = this.entities[markupNode.entityID];
+      return JSON.stringify(variableValue);
+    }
+
+    if (this.isMarkupSpan(markupNode)) {
+      return {
+        attributes: markupNode.attributes,
+        text: this.resolveMarkupCollection(markupNode.text),
+      };
+    }
+
+    throw new Error('received unexpected markup content');
+  }
+
+  private resolveMarkupCollection(markup: Markup): ResolvedMarkup.Markup {
+    return markup.map((node) => this.resolveMarkupNode(node));
+  }
+
+  public resolveMarkup(markup: Markup) {
+    return this.resolveMarkupCollection(markup);
+  }
+
+  public resolve(content: string) {
+    return replaceVariables(content, {
+      ...this.variables,
+      ...this.entities,
+    });
+  }
+}
