582b13ce934491eb8e7372c0cde5f9beddf1ad6b
Brennan Lu <brennan.lu314@gmail.com>
2023-07-19 15:56:55 -0400

    docs: commentary on translation's shortcomings

--- lib/services/runtime/handlers/v3/response/translateVariants/translateVariants.ts
+++ lib/services/runtime/handlers/v3/response/translateVariants/translateVariants.ts
@@ -4,6 +4,12 @@ import { ResolvedVariant } from '../response.types';
 import { TranslationContext } from './translateVariants.interface';
 
 // $TODO$ - Need to ensure that the translation doesn't mess up the markup content
+// $TODO$ - This will be a somewhat difficult problem. We can't translate once the variables are all resolved and
+//          everything is pieced together, because it might mess up the output if we expect an exact variable value
+//          to show up.
+//
+//          However, if we translate before we resolve all the variables, then the translation might be slightly
+//          inaccurate because the translation isn't using the full context.
 export function translateVariants(currContext: TranslationContext, variants: Record<string, ResolvedVariant>) {
   if (currContext.toLang !== currContext.fromLang) {
     throw new VError('Translation is not implemented');
