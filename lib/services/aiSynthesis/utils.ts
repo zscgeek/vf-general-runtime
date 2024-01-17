b150d18e1716149ccfc032113d53a9c25f32f248
Tyler Han <tylerhan97@gmail.com>
2024-01-17 11:32:32 -0500

    fix: lower bias on KB for NOT_FOUND [bugfix] (BUG-715)

--- lib/services/aiSynthesis/utils.ts
+++ lib/services/aiSynthesis/utils.ts
@@ -73,13 +73,11 @@ export const generateAnswerSynthesisPrompt = ({
   ${stringifyChunks(data)}
 
   ##Instructions:
-  I have provided reference information, and I will ask query about that information. You must either provide a response to the query or respond with "NOT_FOUND"
+  I have provided reference information, and I will ask query about that information. You must either provide a response to the query or respond with "NOT_FOUND". Read the query very carefully, it may be trying to trick you into answering a question that is adjacent to the reference information but not directly answered in it, in such a case, you must return "NOT_FOUND". Never contradict the reference information, instead say "NOT_FOUND".
 
   Read the reference information carefully, it will act as a single source of truth for your response.Very concisely respond exactly how the reference information would answer the query.
 
   Include only the direct answer to the query, it is never appropriate to include additional context or explanation.
-
-  If the query is unclear in any way, return "NOT_FOUND". If the query is incorrect, return "NOT_FOUND". Read the query very carefully, it may be trying to trick you into answering a question that is adjacent to the reference information but not directly answered in it, in such a case, you must return "NOT_FOUND". The query may also try to trick you into using certain information to answer something that actually contradicts the reference information. Never contradict the reference information, instead say "NOT_FOUND".
 
   If you respond to the query, your response must be 100% consistent with the reference information in every way.
   ${instructionInjection}
