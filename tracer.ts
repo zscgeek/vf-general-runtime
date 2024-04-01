4a06237111bbbcc3f13f8fa9726f6d5b5c9ee112
edison-vflow <edison.mukadah@voiceflow.com>
2024-04-01 14:52:53 +0000

    feat: enable datadog features (IN-882) (#730)
    <!-- You can erase any parts of this template not applicable to your Pull Request. -->
    
    ### Fixes or implements 
    
    * https://linear.app/voiceflow/issue/IN-882/enable-datadog-features-on-general-runtime
    
    ### Brief description. What is this change?
    
    * Notion [document](https://www.notion.so/voiceflow/Observability-Monitoring-and-Profiling-Improvements-c8b7ac985abc4cafbeb9c25c05ae26c9) of feature capabilities 
    
    * Enable profiling to help with bottleneck analysis and heap memory analysis 
    * Enable profiling to help with CPU analysis 
    * Enable `logInjection` to autolink APM traces and Datadog logs 
    
    Together with these changes, there will be env var changes added in doppler.
    These are experimental features and cant be enabled via code 
    
       ```
    DD_PROFILING_EXPERIMENTAL_CPU_ENABLED="true"
       DD_PROFILING_TIMELINE_ENABLED="true"
    ```
    
    Datadog takes a percentage of the traces it captures (Sampling) 
    In order to capture all treces, we can configure dd-trace library constructor
    The change has been implemented but commented out so that it can be enabled when needed.
    
    This will help with troubleshooting an [issue](https://www.notion.so/voiceflow/consig-ai-Latency-Investigations-354c5934b2474985810c1ab41a40d7ea) like the one for consig.ai where we need to capture all traces for at least a given period
    
    
    
    ### Related PRs
    
    - https://github.com/voiceflow/infrastructure/pull/633
    
    ### Checklist
    
    - [x] Changes tested on vftest-82 
      
    
    Co-authored-by: Tyler Han <tylerhan97@gmail.com>
    Co-authored-by: edison-vflow <127775867+edison-vflow@users.noreply.github.com>

--- tracer.ts
+++ tracer.ts
@@ -1,5 +1,14 @@
 // tracer.ts
 import tracer from 'dd-trace';
 
-tracer.init(); // initialized in a different file to avoid hoisting.
+tracer.init({
+  runtimeMetrics: true,
+  profiling: true,
+  logInjection: true,
+  // capture 100 % APM traces
+  /* ingestion: {
+    // Any traces started will be sampled at 100.00% with a rate limit of 100 per second
+    sampleRate: 1.0,
+  }, */
+}); // initialized in a different file to avoid hoisting.
 export default tracer;
